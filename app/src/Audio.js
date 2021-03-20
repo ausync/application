9fe32786ab16bb5a25b61213a45b5f360e8e8c25
Ihsan <>
2021-03-20 08:04:08 +0100

    UI audio page

--- app/src/Audio.js
+++ app/src/Audio.js
@@ -74,35 +74,57 @@ function AudioDetail() {
     const { audioId } = useParams();
 
     window.Tone = Tone;
-    const [isLoaded, setLoaded] = useState(false);
-    const resource = useRef(null);
+    const [resource, setResource] = useState(null);
 
     useEffect(async () => {
         getData(audioId).then((data) => {
-            resource.current = data.script;
+            setResource(data);
             console.log("data from ipfs: ", data);
         }).catch( err => {
             alert("Error occurred: ", err);
-
-        }).finally( () => {
-            setLoaded(true);
         });
     }, []);
 
     const handleClick = () => {
-        eval(resource.current);
+        eval(resource.script);
         //oscillator();
     };
 
     return (
-        <div>
-            {(!isLoaded)?
+        <div className="container">
+            {(resource == null)?
             <p><em>Loading...</em></p>:
-            <button disabled={!isLoaded} onClick={handleClick}>
-                start layer music
-            </button>}
-            <audio src="/A1.mp3" id="audio" controls="">audio element not supported</audio>
-            { //<canvas id="canvas" width="800" height="350"></canvas>
+            <div className="mt-4">
+                <div className="row">
+                    <h5 className="h5">Audio layer</h5>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div>
+                            <img src="/wave2.png" height="135px"/>
+                        </div>
+                        <div>
+                            <button onClick={handleClick} className="btn btn-success">
+                                start the music
+                            </button>
+                        </div>
+                        <div className="mt-5">
+                            <button onClick={handleClick} className="btn btn-light">
+                                show on blockchain TODO
+                            </button>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <h1 className="h1">{resource.title}</h1>
+                        <p className="p-3">{resource.description}</p>
+                        <p className="p-3"><b>Created on </b> {new Date(resource.created).toDateString()}</p>
+                        <p className="p-3"><i>{resource.script.split("\n").length} line{resource.script.split("\n").length > 1? "s": ""} of code</i></p>
+                    </div>
+                </div>
+            </div>
+            }
+
+            { // <canvas id="canvas" width="800" height="350"></canvas>
             }
         </div>
     );
